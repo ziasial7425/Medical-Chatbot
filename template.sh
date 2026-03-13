d30414058994c2d74d4685a4b7cc952d0e58370c
Zia Ur Rehman Sial <sialziaurrehman@gmail.com>
2026-03-14 03:02:41 +0500

    Structure Created

---
+++ template.sh
@@ -0,0 +1,15 @@
+#  creating directries
+mkdir -p src
+mkdir -p research
+
+# creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Project structure created successfully!"
