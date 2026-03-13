48d557b2cddb6bd1a8a57f9a49366dd5440a987d
Zia Ur Rehman Sial <sialziaurrehman@gmail.com>
2026-03-14 03:24:41 +0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    author="Zia Sial",
+    author_email="sialziaurrehman@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
